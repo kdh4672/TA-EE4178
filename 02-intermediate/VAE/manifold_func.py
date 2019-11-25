e06843f4db94e172c8e08093e9042ee72ce76f5e
Sangbuem Seo <gamchanr@gmail.com>
2019-11-25 20:12:19 +0900

    Update manifold_func.py

--- 02-intermediate/VAE/manifold_func.py
+++ 02-intermediate/VAE/manifold_func.py
@@ -1,7 +1,7 @@
 import numpy as np
 
 
-def plt_manifold(trained_model_instance, mean_range=3, n=20, figsize=(8, 10)): #
+def plt_manifold(trained_model_instance, save_file_path, mean_range=3, n=20, figsize=(8, 10)): #
     x_axis = np.linspace(-mean_range, mean_range, n)
     y_axis = np.linspace(-mean_range, mean_range, n)
     canvas = np.empty((28*n, 28*n))
@@ -16,13 +16,14 @@ def plt_manifold(trained_model_instance, mean_range=3, n=20, figsize=(8, 10)): #
     plt.figure(figsize=figsize)
     xi, yi = np.meshgrid(x_axis, y_axis)
     plt.imshow(canvas, origin="upper")
-    plt.savefig(manifold_image)
+    plt.savefig(save_file_path)
     plt.show()
 
     return
 
-""" 적용예시
+""" 적용 예시
+save_file_path = "./MNIST_manifold.png"
 
 with torch.no_grad():
-    plt_manifold(model)
+    plt_manifold(model, save_file_path)
 """
